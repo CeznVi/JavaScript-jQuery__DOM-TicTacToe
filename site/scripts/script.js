5ed1ce1e8173f587f6aa559aa69b02ab50c87711
Viktor Velychko <Cezn.Vi@gmail.com>
2023-09-16 21:33:24 +0300

    W I P

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -53,24 +53,127 @@ function clearTable() {
 
 function turnClick(event) {
 	turn(event.target, playerX)
-		//if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
+    
+    if (!checkWin(origBoard, playerX) && !checkDraw()) {
+        turn(bestSpot(), playerO);
+    }
+
 }
 
 function turn(eId, player) {
-	
     origBoard[eId] = player;
 
     if(player == playerX) {
         eId.classList.add('xFig');
     } else if(player == playerO) {
-        eId.classList.add('oFig');
-
+        cells[eId].classList.add('oFig');
     }
 
-	// let gameWon = checkWin(origBoard, player)
-	// if (gameWon) gameOver(gameWon)
+	let gameWon = checkWin(origBoard, player);
+	
+    if (gameWon) {
+        gameOver(gameWon)
+    } 
 }
 
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function checkDraw() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("НИЧЬЯ!")
+		return true;
+	}
+	return false;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return minimax(origBoard, playerO).index;
+}
+
+function minimax(newBoard, player) {
+	var availSpots = emptySquares();
+
+	if (checkWin(newBoard, playerX)) {
+		return {score: -10};
+	} else if (checkWin(newBoard, playerO)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+
+		if (player == playerO) {
+			var result = minimax(newBoard, playerX);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, playerO);
+			move.score = result.score;
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === playerO) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
+}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		cells[index].style.backgroundColor =
+			gameWon.player == playerX ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == playerX ? "Вы выграли!" : "Вы проиграли!");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+}
 //// * ------------------------------------ Инициализация -----------------------------------------------
 
 
