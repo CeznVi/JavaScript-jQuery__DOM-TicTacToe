7f1731408b88096170497bd22432c2cd1917a0fc
Viktor Velychko <Cezn.Vi@gmail.com>
2023-09-16 20:17:09 +0300

    W I P

--- site/scripts/script.js
+++ site/scripts/script.js
@@ -1,57 +1,28 @@
 //// * ------------------------------------ ПЕРЕМЕННЫЕ -----------------------------------------------
 var table = document.getElementById('tictactoe');
-var cells = table.getElementsByTagName('td');
-var rows = table.getElementsByTagName('tr');
-
-cells = [];
-
+var cells = table.getElementsByClassName('cell');
 var contrCont = document.querySelector('.controls-container');
 var startButton = contrCont.querySelector('input[type="button"]');
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+var origBoard;
 //// * ------------------------------------ Функции -----------------------------------------------
 
-
-//// * ------------------------------------ Инициализация -----------------------------------------------
-
-if (table == null) {
-    console.error('EROR');
+function startGame() {
+	document.querySelector(".endgame").style.display = "none";
+	origBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].addEventListener('click', turnClick, false);
+	}
 }
-
-for(let i = 0; i < rows.length; i++)
-{
-    cells[i] = rows[i].getElementsByTagName('td');
-}
-
-for(let r= 0; r < cells.length; r++) {
-    for(let c=0; c<cells[r].length; c++){
-        cells[r][c].addEventListener('mouseover', (e) => {
-            e.target.classList.add('selected');
-        });
-        cells[r][c].addEventListener('mouseout', (e) => {
-            e.target.classList.remove('selected');
-        });
-
-        cells[r][c].addEventListener('click', (e) => {
-            e.target.classList.add('xFig');
-        });
-    }
-}
-
-startButton.addEventListener('click', ()=> {
-    var loginField = contrCont.querySelector('input[name = "Login"]');
-    var gameModeField = contrCont.querySelector('select[name = "gameMode"]')
-    
-
-    var userLogin = loginField.value;
-    var userMode = gameModeField.options[gameModeField.selectedIndex].value;
-    //alert(userLogin + " " + userMode);
-    checkStyleDisplayTable();
-    clearTable();
-
-
-
-
-});
-
 
 function checkStyleDisplayTable() {
     if (table.style.display != "table") { 
@@ -60,8 +31,51 @@ function checkStyleDisplayTable() {
 }
 
 function clearTable() {
-    
+    for(let i = 0; i < cells.length; i++) {
+        cells[i].classList.remove('xFig');
+        cells[i].classList.remove('oFig');
+    }
+}
+
+function turnClick(square) {
+	if (typeof origBoard[square.target.id] == 'number') {
+		turn(square.target.id, huPlayer)
+		if (!checkWin(origBoard, huPlayer) && !checkTie()) turn(bestSpot(), aiPlayer);
+	}
+}
+
+//// * ------------------------------------ Инициализация -----------------------------------------------
+
+
+startButton.addEventListener('click', ()=> {
+    var loginField = contrCont.querySelector('input[name = "Login"]');
+    var gameModeField = contrCont.querySelector('select[name = "gameMode"]')
+    var userLogin = loginField.value;
+    var userMode = gameModeField.options[gameModeField.selectedIndex].value;
+    checkStyleDisplayTable();
+    clearTable();
+    startGame();
+});
+
+//// * ------------------------------------ Игровой процесс -----------------------------------------------
+
+if (table == null) {
+    console.error('EROR');
 }
 
 
 
+
+
+/// работает
+// for(let i = 0; i < cells.length; i++) {
+//         cells[i].addEventListener('mouseover', (e) => {
+//             e.target.classList.add('selected');
+//         });
+//         cells[i].addEventListener('mouseout', (e) => {
+//             e.target.classList.remove('selected');
+//         });
+//         cells[i].addEventListener('click', (e) => {
+//             e.target.classList.add('xFig');
+//         });
+// }
